a0ed7055eaaf39c40c4832f834006c24e16e5f2b
saagor16 <saagor.16@gmail.com>
2024-12-05 18:17:00 +0600

    Created routes for item management

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const app = express()
+const app = express();
+const cors = require('cors')
 const port = process.env.POT || 5000;
 
 
 //saagor16
 //C6K2KmONj5pbGdd5
 
+app.use(express.json());
+app.use(cors());
 
 
 async function main() {
@@ -19,6 +22,9 @@ async function main() {
 
 main().then(()=>console.log('mongodb connected successfully')).catch(err => console.log(err));
 
+//routes
+const ItemRoutes =require("./src/routes/itemRoute");
+app.use('/api',ItemRoutes)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
