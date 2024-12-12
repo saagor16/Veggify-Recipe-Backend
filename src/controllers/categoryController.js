2c6840413e457e83125c516ad30b15062b2cf35c
saagor16 <saagor.16@gmail.com>
2024-12-12 17:03:26 +0600

    Fixed category controller by adding missing await for asynchronous operations

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -9,7 +9,7 @@ const getCategory = async(req,res)=>{
         if (!categoryData) {
             return res.status(404).json({message : "category not found!"})
         }
-        const items =Item.find({menuId : categoryData.menuId})
+        const items =await Item.find({menuId : categoryData.menuId})
         res.status(200).json(items)
 
     } catch (error) {
